23eeba8898ec761140b61f6f649ad645d4b4aa8a
Lucas Satabin <lucassat@n7mm.org>
2010-10-17 22:53:05 +0200

    old code removed

--- library/gnieh/coroutines/coroutines.scala
+++ library/gnieh/coroutines/coroutines.scala
@@ -2,25 +2,6 @@ package gnieh.coroutines
 
 import scala.util.continuations._
 
-object Coroutine {
-  abstract class create[P, R] extends Coroutine[P, R]
-
-  abstract class wrap[P, R] extends Function1[P, R] { self =>
-    private[this] var _fun: P => R /*@cpsParam[R,R]*/ = null
-    def fun = _fun
-    def fun_=(f: P => R /*@cpsParam[R,R]*/ ) {
-      _fun = f
-      c.fun = f
-    }
-    lazy val c = new create[P, R] { var fun = self.fun }
-    def apply(p: P): R = {
-      /* resume coroutine */
-      c.resume(p)
-    }
-  }
-
-}
-
 class ConsumedCoroutineException(msg: String) extends Exception(msg)
 
 abstract class Coroutine[Param, Ret] {
