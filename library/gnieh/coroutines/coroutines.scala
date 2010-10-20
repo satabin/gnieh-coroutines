dad0b8877e23e966b737af051840eb349d8af318
Lucas Satabin <lucassat@n7mm.org>
2010-10-21 01:44:20 +0200

    license added

--- library/gnieh/coroutines/coroutines.scala
+++ library/gnieh/coroutines/coroutines.scala
@@ -1,3 +1,19 @@
+/* This file is part of gnieh-coroutines.
+*
+* See the NOTICE file distributed with this work for copyright information.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package gnieh.coroutines
 
 import scala.util.continuations._
@@ -11,25 +27,25 @@ abstract class Coroutine[Param, Ret] {
   }
 
   var fun: Param => Ret = shot
-  
+
   def resume(p: Param): Ret = {
     fun(p)
   }
-  
+
   def yld(v: Ret) = {
-    shift {k: (Param => Ret) =>
+    shift { k: (Param => Ret) =>
       fun = k
       v
     }
   }
-  
+
   def ret(v: Ret) = {
-    shift {k: (Unit => Unit) =>
+    shift { k: (Unit => Unit) =>
       fun = shot
       v
     }
   }
-  
+
 }
 
 // vim: set ts=4 sw=4 et:
--- library/gnieh/coroutines/package.scala
+++ library/gnieh/coroutines/package.scala
@@ -1,11 +1,27 @@
+/* This file is part of gnieh-coroutines.
+*
+* See the NOTICE file distributed with this work for copyright information.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package gnieh
 
 import scala.util._
 import continuations._
 
 package object coroutines {
-  def fun[Param,Ret](p: Param): Ret = throw new NoSuchMethodException("this code has to be compiled with the Scala coroutines plugin enabled")
-  def yld[T,V](v: T): V = throw new NoSuchMethodException("this code has to be compiled with the Scala coroutines plugin enabled")
+  def fun[Param, Ret](p: Param): Ret = throw new NoSuchMethodException("this code has to be compiled with the Scala coroutines plugin enabled")
+  def yld[T, V](v: T): V = throw new NoSuchMethodException("this code has to be compiled with the Scala coroutines plugin enabled")
   def create[Param, Ret](fun: Param => Ret): Coroutine[Param, Ret] = throw new NoSuchMethodException("this code has to be compiled with the Scala coroutines plugin enabled")
   def wrap[Param, Ret](fun: Param => Ret): Param => Ret = throw new NoSuchMethodException("this code has to be compiled with the Scala coroutines plugin enabled")
 }
